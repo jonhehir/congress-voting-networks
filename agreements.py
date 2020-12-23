616de266219653eb4d875a10c60aa3eb867e182f
JH <jh@psu.edu>
2020-12-23 11:45:06 -0500

    perf improvements

--- agreements.py
+++ agreements.py
@@ -1,28 +1,21 @@
 from collections import Counter, defaultdict
+import logging
+import multiprocessing
 
 
-def votes_agree(a, b):
-    # out of range (includes "not a member of chamber when vote was taken")
-    if min(a, b) < 1 or max(a, b) > 9:
-        return False
-    
-    # two yeas
-    if a in [1, 2, 3] and b in [1, 2, 3]:
-        return True
-    
-    # two nay/abstain/present
-    if a >= 4 and b >= 4:
-        return True
-    
-    return False
+def translate_vote(code):
+    if code in [1, 2, 3]:
+        return True # yea
+    if code in [4, 5, 6, 7, 8, 9]:
+        return False # nay
+    return None # doesn't count
 
-def tally_agreements(filename):
-    agreements = Counter()
+def read_votes(filename):
+    # session => Tuple[rollcall, vote] => List[voters]
     votes = defaultdict(lambda: defaultdict(list))
     
     past_headers = False
-    #lines = 0
-
+    
     with open(filename) as f:
         for line in f:
             # skip header line
@@ -30,25 +23,43 @@ def tally_agreements(filename):
                 past_headers = True
                 continue
             
-            #lines += 1
-            #if lines > 100000:
-                #break # just quit early for now
-            
             fields = line.strip().split(",")
             session, rollcall, voter, vote = int(fields[0]), int(fields[2]), int(fields[3]), int(fields[4])
-            votes[session][rollcall].append((voter, vote))
+            vote = translate_vote(vote)
+            if vote is not None:
+                votes[session][(rollcall, vote)].append(voter)
     
-    for session in votes:
-        for rollcall, tuples in votes[session].items():
-            for i in range(len(tuples)):
-                for j in range(i+1, len(tuples)):
-                    if votes_agree(tuples[i][1], tuples[j][1]):
-                        a = min(tuples[i][0], tuples[j][0])
-                        b = max(tuples[i][0], tuples[j][0])
-                        agreements[(session, a, b)] += 1
+    return dict(votes) # don't care about defaultdict anymore, causes pickling problems
+
+def tally_agreements(votes, session):
+    agreements = Counter()
+    
+    for voters in votes[session].values():
+        for i in range(len(voters)):
+            for j in range(i+1, len(voters)):
+                a, b = min(voters[i], voters[j]), max(voters[i], voters[j])
+                agreements[(session, a, b)] += 1
     
     return agreements
+    
+def print_agreements(l):
+    for agreements in l:
+        for ids, count in agreements.items():
+            print("\t".join(map(str, list(ids) + [count])))
+
+def print_error(e):
+    print(e)
 
 
-for ids, count in tally_agreements("Hall_votes.csv").items():
-    print("\t".join(map(str, list(ids).append(count))))
+if __name__ == "__main__":
+    # read in votes (takes a bit, ~500 MB)
+    votes = read_votes("Hall_votes.csv")
+
+    # tally and print agreements (also takes a bit)
+    multiprocessing.log_to_stderr()
+    logger = multiprocessing.get_logger()
+    logger.setLevel(logging.INFO)
+    pool = multiprocessing.Pool()
+    pool.starmap_async(tally_agreements, [(votes, k) for k in votes.keys()], callback=print_agreements, error_callback=print_error)
+    pool.close()
+    pool.join()
